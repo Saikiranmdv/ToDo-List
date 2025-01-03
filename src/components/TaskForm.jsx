bd596f1a336c2947f5497ce90a4cb9fcf812b049
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2025-01-03 14:20:00 +0530

    adding colour when selected

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -8,8 +8,28 @@ const TaskForm = ({ setTasks }) => {
   // another way to set useState instead of different useState variables
   const [TaskData, setTaskData] = useState({
     task: "",
-    status: "todo"
+    status: "todo",
+    tags: []
   })
+
+  const checkTag = (tag) => {
+    return TaskData.tags.some(item => item === tag)
+  }
+
+  const selectTag = (tag)=> {
+    if(TaskData.tags.some(item => item === tag)){
+      const filterTag =  TaskData.tags.filter(item => item !==tag)
+      setTaskData(prev => {
+        return { ...prev, tags: filterTag}
+      })
+    }else{
+      setTaskData(prev => {
+        return {...prev, tags :[...prev.tags, tag]}
+      })
+    }
+  }
+
+console.log(TaskData.tags)
 
   const handleChange = (e) => {
     const { name , value } = e.target
@@ -20,7 +40,6 @@ const TaskForm = ({ setTasks }) => {
 
   const handleSubmit = (e) =>{
     e.preventDefault();
-    console.log(TaskData);
   }
   // const [Task, setTask] = useState("");
   // const [Status, setStatus] = useState("todo")
@@ -41,10 +60,10 @@ const TaskForm = ({ setTasks }) => {
          <input type="text" name="task" className="task_input" placeholder="Enter a task" onChange={handleChange}/>
          <div className="task_form_bottom_line">
            <div>
-             <Tag tagName="HTML" />
-             <Tag tagName="CSS" />
-             <Tag tagName="JavaScript"/>
-             <Tag tagName="React"/>           
+             <Tag tagName="HTML" selectTag = {selectTag} selected={checkTag("HTML")} />
+             <Tag tagName="CSS" selectTag = {selectTag} selected={checkTag("CSS")}/>
+             <Tag tagName="JavaScript" selectTag = {selectTag} selected={checkTag("JavaScript")}/>
+             <Tag tagName="React" selectTag = {selectTag} selected={checkTag("React")}/>           
           </div>
 
            <div>
